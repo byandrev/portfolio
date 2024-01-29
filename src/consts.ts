4669c8efbfb648708d998300a50d79c15dd96d68
Andres Parra <byandrev@gmail.com>
2024-01-29 14:00:44 -0500

    create about me

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'byandrev';
-export const SITE_DESCRIPTION = 'Andres Parra - Software Enginner';
+export const SITE_TITLE = 'Andres Parra - 👨‍💻 Software Engineer';
+export const SITE_DESCRIPTION = 'Andres Parra - Software Engineer';
